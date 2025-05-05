dbf0c09e39ccaa110c192666fbdf54daec05d2ca
kamonphankanthayod <kamonphan.main@gmail.com>
2025-05-05 20:18:39 +0700

    add index.mjs file && add document in README.md && add pdf file -- by Pe

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # AWS_project
+
+**Lambda Endpoint URL**  
+📍 `GET https://oylr4xqzr6agtddkqbzz5wedde0lxlvz.lambda-url.us-east-1.on.aws/`
+
+**Description:**  
+เช็ควัตถุดิบที่ใกล้หมดอายุภายใน 3 วันจาก DynamoDB
+
+---
+
+### ✅ ตัวอย่าง Response
+
+```json
+{
+  "message": "Expiring ingredients found:",
+  "count": 3,
+  "items": [
+    { "name": "นมสด", "expiry_date": "2025-05-05" },
+    { "name": "ไข่ไก่", "expiry_date": "2025-05-08" }
+  ]
+}
---
+++ index.mjs
@@ -0,0 +1,52 @@
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+
+const client = new DynamoDBClient({});
+
+export const handler = async () => {
+  const tableName = 'Ingredients';         //table name  !!!improtant
+  const thresholdDays = 3;                 //can edit value 
+
+  const today = new Date();
+  const thresholdDate = new Date(today);
+  thresholdDate.setDate(today.getDate() + thresholdDays);
+
+  const params = {
+    TableName: tableName
+  };
+
+  try {
+    const command = new ScanCommand(params);
+    const result = await client.send(command);
+    const expiringItems = [];
+
+    result.Items.forEach(item => {
+      const name = item.name.S;
+      const expiry_date = item.expiry_date.S;
+      const expiry = new Date(expiry_date);
+
+      if (expiry <= thresholdDate) {
+        expiringItems.push({ name, expiry_date });
+      }
+    });
+
+    const response = {
+      message: expiringItems.length > 0
+        ? "Expiring ingredients found:"
+        : "No expiring ingredients found.",
+      count: expiringItems.length,
+      items: expiringItems
+    };
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    };
+    
+  } catch (err) {
+    console.error("Error scanning table:", err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
