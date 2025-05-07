8f4ba47457c1187ac0536a1aa295eeb9926f5509
kamonphankanthayod <kamonphan.main@gmail.com>
2025-05-07 23:21:02 +0700

    update index.mjs - add headers in response

--- index.mjs
+++ index.mjs
@@ -39,9 +39,14 @@ export const handler = async () => {
 
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
       body: JSON.stringify(response)
     };
-    
+
   } catch (err) {
     console.error("Error scanning table:", err);
     return {
