9085b3c2be4ca8753395c54ff0e088c0b278cfb1
newbie <thanagon.pay@dome.tu.ac.th>
2025-05-13 22:34:54 +0700

    boss_config

--- public/js/register.js
+++ public/js/register.js
@@ -1,44 +1,49 @@
 function signup() {
-    const username = document.getElementById('username').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const password = document.getElementById('password').value.trim();
-    const confirmPassword = document.getElementById('confirm-password').value.trim();
+  const username = document.getElementById("username").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const confirmPassword = document
+    .getElementById("confirm-password")
+    .value.trim();
 
-    if (!username || !email || !password || !confirmPassword) {
-        alert("Please fill all fields.");
-        return;
-    }
+  if (!username || !email || !password || !confirmPassword) {
+    alert("Please fill all fields.");
+    return;
+  }
 
-    if (password !== confirmPassword) {
-        alert("Passwords do not match.");
-        return;
-    }
+  if (password !== confirmPassword) {
+    alert("Passwords do not match.");
+    return;
+  }
 
-    showToast("Create accounting...")
+  showToast("Create accounting...");
 
-    //backend (เปลี่ยน URL API)
-    fetch('https://ak6gm44y14.execute-api.us-east-1.amazonaws.com/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username: username,
-            email: email,
-            password: password
-        })
+  //backend (เปลี่ยน URL API)
+  fetch(
+    "https://0d74mxdrlf.execute-api.us-east-1.amazonaws.com/newbitebright/register-login",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        email: email,
+        password: password,
+      }),
+    },
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Sign up failed.");
+      }
+      return response.json();
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Sign up failed.");
-        }
-        return response.json();
+    .then((data) => {
+      // alert("Sign up success! Please login.");
+      window.location.href = "login.html";
     })
-    .then(data => {
-        // alert("Sign up success! Please login.");
-        window.location.href = "login.html";
-    })
-    .catch(error => {
-        alert(error.message);
+    .catch((error) => {
+      alert(error.message);
     });
 }
